e9c2ad0c6706326144691ede0abb933d4921fa8d
Marcos Pérez <marcosperez@Marcos-Perez.local>
2020-07-15 11:32:44 +0200

    Ejecricio 1

---
+++ Ejercicio 1/Ejercicio1.py
@@ -0,0 +1,18 @@
+#Escribir 3 numeros y sacar por pantalla su tipo, 
+# calcular la suma de los numeros y sacar por pantalla el valor y el tipo
+
+numero1 = 5
+numero2 = 6.5
+numero3 = 7
+
+
+# print(type(numero1))
+# print(type(numero2))
+# print(type(numero3))
+
+suma = numero3 + numero1 + numero2
+
+print(type(suma))
+print(suma)
+
+
