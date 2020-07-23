479183dff1fc03a18490a20f38131c3f2ed7198c
Marcos Pérez <marcosperez@Marcos-Perez.local>
2020-07-23 10:36:27 +0200

    Colecciones de datos

---
+++ Colecciones de datos/Ejercicio1.py
@@ -0,0 +1,10 @@
+# Dada la siguiente lista = [1,2,5,25,33,56,71,21,56,89,43,13,62,24]
+# Mostrar mediante el metodo "print" y el operador "in" si el numero 21 esta en la lista
+
+lista = [1,2,5,25,33,56,71,21,56,89,43,13,62,24]
+numero = 21
+
+if numero in lista:
+    print("Esta en la lista")
+else:
+    print("No esta en la lista")
