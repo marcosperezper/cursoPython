01b4380c617676eaa4cab5cae9b4a3626f05462f
Marcos Pérez <marcosperez@Marcos-Perez.local>
2020-07-23 09:08:17 +0200

    Operadores

---
+++ Operadores/Ejercicio1.py
@@ -0,0 +1,19 @@
+# Crear una variable "nota1" que tenga el valor 6
+# Crear una variable "nota2" que tenga el valor 4
+# Crear una varibale "nota3" que tenga el valor 7
+# Crear una varibale "nota_media" que tenga el valor promedio de las 3 notas anteriores
+# Crear una varibale "nota_final" que tenga el valor "aprobado" (mayor o igual que 5)
+
+
+nota1 = 6
+nota2 = 4
+nota3 = 7
+
+nota_media = (nota1 + nota2 +nota3)/3
+print(nota_media)
+
+if nota_media >= 5:
+    nota_final = "Aprobado"
+
+print(nota_final)
+
