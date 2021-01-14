6ef42e619ac5dd743a68f815eafb0215a5487b93
Marcos Pérez <marcosperez@Marcos-Perez.local>
2021-01-14 14:21:07 +0100

    Variables y numeros ejercicio2

---
+++ Variables y numeros/Ejercicio2.py
@@ -0,0 +1,19 @@
+#Crea la variable "numero1" con el valor 5
+#Crea la variabe "numero2" con el valor 3
+#Crea la variable "suma" con el valor de la suma entre "numero1" y "numero2"
+#Convertir la variable "suma" en una cadena de carcteres y llamarla "strSuma"
+#Crear una variable "resultado" que concatene el texto "El resultado es " y la variable "strSuma"
+
+
+numero1 = 5
+numero2 = 3
+
+suma =  numero1 + numero2
+
+strSuma = str(suma)
+
+texto = "El resultado es: "
+
+resultado = texto + strSuma
+
+print(resultado)
